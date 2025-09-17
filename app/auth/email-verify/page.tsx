2e32c364afe0e07014dc493b42cdaa9f7fc7b39d
yazshrilld <yazshrilld@gmail.com>
2025-09-17 14:55:47 +0100

    updated the code for server component to be withuout search params

--- app/auth/email-verify/page.tsx
+++ app/auth/email-verify/page.tsx
@@ -1,15 +1,10 @@
 import React, { Suspense } from "react";
-import { useSearchParams, useRouter } from "next/navigation";
-import { useMutation } from "@tanstack/react-query";
-import { emailVerifyFn } from "@/utils/ApiFactory/auth";
 import EmailVerifyClient from "./EmailVerifyClient";
 
 export default function EmailVerifyPage() {
   return (
-    <Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
       <EmailVerifyClient />
     </Suspense>
   );
 }
-
-// Removed the previous EmailVerifyPage component code as it's now in EmailVerifyClient
