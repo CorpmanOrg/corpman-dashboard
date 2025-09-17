4b8df1335c662b3f98678e71ea613d418cbbb972
yazshrilld <yazshrilld@gmail.com>
2025-09-17 15:48:06 +0100

    updated the code for suspense errror

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import { Suspense } from "react";
 import Image from "next/image";
 import BkgImage from "../../components/assets/img/auth-bkdImg.png";
 import SignupForm from "@/app/auth/SignupForm";
@@ -6,12 +8,21 @@ import SignupForm from "@/app/auth/SignupForm";
 const SignUp = () => {
   return (
     <div className="relative min-h-screen w-full">
-      <Image src={BkgImage} alt="Background" fill className="object-cover -z-10" />
+      <Image
+        src={BkgImage}
+        alt="Background"
+        fill
+        className="object-cover -z-10"
+        priority
+      />
 
       <div className="absolute inset-0 backdrop-blur-[3px] bg-white/30 -z-0" />
+
       <div className="relative z-10 flex items-center justify-center min-h-screen">
         <div className="w-[700px] bg-white/40 backdrop-blur-md border border-white/50 shadow-lg rounded-lg px-6 py-10">
-          <SignupForm />
+          <Suspense fallback={<div className="text-center">Loading...</div>}>
+            <SignupForm />
+          </Suspense>
         </div>
       </div>
     </div>
